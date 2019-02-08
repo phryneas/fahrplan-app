1fb660f72488beac072915bf31471a8ef66a79c8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-08 11:00:57 +0000

    Deploy WueWW/fahrplan-app to github.com/WueWW/fahrplan-app.git:gh-pages

---
+++ precache-manifest.e83d372fa55ed5f671e63e29f6297647.js
@@ -0,0 +1,159 @@
+self.__precacheManifest = [
+  {
+    "revision": "70612fb38fd9799d390d",
+    "url": "styles.css"
+  },
+  {
+    "revision": "70612fb38fd9799d390d",
+    "url": "./app.js"
+  },
+  {
+    "url": "cd6c777f1945164224dee082abaea03a.woff2"
+  },
+  {
+    "url": "ef60a4f6c25ef7f39f2d25a748dbecfe.woff"
+  },
+  {
+    "url": "flags.png?9c74e172f87984c48ddf5c8108cabe67"
+  },
+  {
+    "url": "outline-icons.eot?701ae6abd4719e9c2ada3535a497b341"
+  },
+  {
+    "url": "outline-icons.ttf?ad97afd3337e8cda302d10ff5a4026b8"
+  },
+  {
+    "url": "0ab54153eeeca0ce03978cc463b257f7.woff2"
+  },
+  {
+    "url": "faff92145777a3cbaf8e7367b4807987.woff"
+  },
+  {
+    "url": "icons.eot?8e3c7f5520f5ae906c6cf6d7f3ddcd19"
+  },
+  {
+    "url": "icons.ttf?b87b9ba532ace76ae9f6edfe9f72ded2"
+  },
+  {
+    "url": "outline-icons.svg?82f60bd0b94a1ed68b1e6e309ce2e8c3"
+  },
+  {
+    "url": "icons.svg?962a1bf31c081691065fe333d9fa8105"
+  },
+  {
+    "revision": "0f6dc22ab9c522155c32551153f030ef",
+    "url": "index.html"
+  },
+  {
+    "revision": "f37c17d9e902b171c6bd392eb1849496",
+    "url": "assets/android-icon-36x36.png"
+  },
+  {
+    "revision": "39025ef95fa2c54ec20f75eedc8aa986",
+    "url": "assets/android-icon-48x48.png"
+  },
+  {
+    "revision": "e755c9e0e7f4c90bc4026ff4ac5de0f3",
+    "url": "assets/android-icon-72x72.png"
+  },
+  {
+    "revision": "c26f30cbaab3a376473f025f30367839",
+    "url": "assets/apple-icon-57x57.png"
+  },
+  {
+    "revision": "f7093c6fd2a6ae6fcdfc34f14ea95457",
+    "url": "assets/apple-icon-60x60.png"
+  },
+  {
+    "revision": "e755c9e0e7f4c90bc4026ff4ac5de0f3",
+    "url": "assets/apple-icon-72x72.png"
+  },
+  {
+    "revision": "5f4bad3820d368a97242c10a0de382aa",
+    "url": "assets/apple-icon-76x76.png"
+  },
+  {
+    "revision": "653d077300a12f09a69caeea7a8947f8",
+    "url": "assets/browserconfig.xml"
+  },
+  {
+    "revision": "31f2f1b20becfe9e7567cd0a913ab43e",
+    "url": "assets/favicon-16x16.png"
+  },
+  {
+    "revision": "460804f440ac4e063ef57a38b6fe597f",
+    "url": "assets/favicon-32x32.png"
+  },
+  {
+    "revision": "f1f3248ce85cd4c39dd4709db0abe486",
+    "url": "assets/favicon.ico"
+  },
+  {
+    "revision": "702c1629037299400b80d7b9a0951c1c",
+    "url": "assets/manifest.json"
+  },
+  {
+    "revision": "6a8fdfaf12e55375b8980faa6825efea",
+    "url": "assets/ms-icon-70x70.png"
+  },
+  {
+    "revision": "267ae14dc0ba484097ce8854a752dcaf",
+    "url": "assets/wueww-logo-2019.svg"
+  },
+  {
+    "revision": "4e8c6e464d51d4ecd1a68a0ffaa85e3c",
+    "url": "assets/android-icon-96x96.png"
+  },
+  {
+    "revision": "d70b9cef5be383125c02f4091773daf6",
+    "url": "assets/apple-icon-114x114.png"
+  },
+  {
+    "revision": "16a86076ded567e682a04259b866d170",
+    "url": "assets/apple-icon-120x120.png"
+  },
+  {
+    "revision": "4e8c6e464d51d4ecd1a68a0ffaa85e3c",
+    "url": "assets/favicon-96x96.png"
+  },
+  {
+    "revision": "3050a301991c68ee09922e9a93ccabb3",
+    "url": "assets/android-icon-144x144.png"
+  },
+  {
+    "revision": "3050a301991c68ee09922e9a93ccabb3",
+    "url": "assets/apple-icon-144x144.png"
+  },
+  {
+    "revision": "1658f96c9b48561c25aa221e91f3dc3d",
+    "url": "assets/apple-icon-152x152.png"
+  },
+  {
+    "revision": "3050a301991c68ee09922e9a93ccabb3",
+    "url": "assets/ms-icon-144x144.png"
+  },
+  {
+    "revision": "ec282f5a96922da211a5e8540b8559a7",
+    "url": "assets/ms-icon-150x150.png"
+  },
+  {
+    "revision": "8da85039a356558fa1c0bd3cf8362b2e",
+    "url": "assets/apple-icon-180x180.png"
+  },
+  {
+    "revision": "60f170e2c69c88ea4139b21f71427bf9",
+    "url": "assets/android-icon-192x192.png"
+  },
+  {
+    "revision": "59fe0d25af343b898af9048da32d4b00",
+    "url": "assets/apple-icon-precomposed.png"
+  },
+  {
+    "revision": "59fe0d25af343b898af9048da32d4b00",
+    "url": "assets/apple-icon.png"
+  },
+  {
+    "revision": "90bf781d73b06fdfef4670aebb920732",
+    "url": "assets/ms-icon-310x310.png"
+  }
+];
